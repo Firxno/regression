5f2bc4c5dd7b5a75915e0c639e3f80ef27c61fb6
Firxno <156784848+Firxno@users.noreply.github.com>
2024-01-31 13:37:18 +0100

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -3,41 +3,31 @@ import numpy
 import matplotlib.pyplot as plt
 
 class DataPreparation:
-	def __init__(self, csv_path):
-		"""
-		Cette classe prend en entrée un chemin de fichier csv.
-		Elle split le jeu de donnée en 2 bases 
-		+ une train 75 %
-		+ une test 25 %
-		Ce 2 bases, la classe va les splits en 2 
+    def __init__(self, csv_path):
+        self.dataset_df = pandas.read_csv(csv_path)
+        self.prepare_data()
 
-		+ un vecteur x (qui contient les indexs temporels)
-		+ un vecteyr y (qui contient les valeurs à prédire)
-		En tout cette va extraire 4 arrays.
-		x_train
-		y_train
-		x_test
-		y_test
-		"""
-		self.dataset_df = pandas.read_csv(csv_path)
-		self.dataset_df["month"] = pandas.to_datetime(self.dataset_df["month"])
-		self.prepare_data()
+    def prepare_data(self):
+        number_of_rows = len(self.dataset_df)
+        self.dataset_df["index_mesure"] = numpy.arange(0, number_of_rows, 1)
 
-	def prepare_data(self):
-		number_of_rows = len(self.dataset_df)
-		self.dataset_df["index_mesure"] = numpy.arange(0, number_of_rows, 1)
+        self.dataset_df["Years"] = pandas.to_datetime(self.dataset_df["Years"])
+        self.dataset_df["Years_name"] = self.dataset_df["Years"].dt.strftime('%B')
+        boolean_df = pandas.get_dummies(self.dataset_df["Years_name"])
 
-		dataset_train_df = self.dataset_df.iloc[ : int(number_of_rows*0.75)]
-		dataset_test_df = self.dataset_df.iloc[int(number_of_rows*0.75): ]
+        self.dataset_df = self.dataset_df.join(boolean_df)
 
-		self.x_train = dataset_train_df[['index_mesure']].values
-		self.y_train = dataset_train_df[['passengers']].values
+        self.dataset_train_df = self.dataset_df.iloc[:int(number_of_rows*0.75)]
+        self.dataset_test_df = self.dataset_df.iloc[int(number_of_rows*0.75):]
 
-		self.x_test = dataset_test_df[['index_mesure']].values
-		self.y_test = dataset_test_df[['passengers']].values
+        self.x_train = self.dataset_train_df[['index_mesure'] + list(boolean_df.columns)].values
+        self.y_train = self.dataset_train_df[['Sales']].values
 
+        self.x_test = self.dataset_test_df[['index_mesure'] + list(boolean_df.columns)].values
+        self.y_test = self.dataset_test_df[['Sales']].values
 
-	def show_graph(self):
-		plt.figure(figsize=(15, 6))
-		plt.plot(self.dataset_df["month"], self.dataset_df["passengers"], "o:")
-		plt.show()
+    def show_graph(self):
+        plt.figure(figsize=(15, 6))
+        plt.plot(self.dataset_df["Years"], self.dataset_df["Sales"], "o:")
+        
+        plt.show()
